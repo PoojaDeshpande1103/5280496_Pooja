8112f07d97fd378e62ee22031c9dc1649016daca
5280496_Pooja Deshpande <poojadeshpande2817171@gmail.com>
2025-08-14 15:45:07 +0530

    Add files via upload

---
+++ Week1/countingsort1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int count[100] = {0};
+    for (int i = 0; i < n; i++) {
+        count[arr[i]]++;
+    }
+
+    for (int i = 0; i < 100; i++) {
+        printf("%d ", count[i]);
+    }
+    return 0;
+}
